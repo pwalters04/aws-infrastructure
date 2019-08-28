6517782e6ade3912ee0873b46b1ef8c6068c2640
pwalters04 <pwalters@honeywell.com>
2019-08-28 09:26:12 -0400

    updated cloud-watch with elb code

--- AmazonTerraform/cloud-watch.tf
+++ AmazonTerraform/cloud-watch.tf
@@ -11,3 +11,16 @@ resource "aws_cloudwatch_metric_alarm" "CPU-Monitoring" {
   alarm_description         = "This metric monitors ec2 cpu utilization"
   insufficient_data_actions = []
 }
+
+resource "aws_cloudwatch_metric_alarm" "ELB-Monitoring" {
+  alarm_name                = "EC2-CPU-Monitoring"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "UnHealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors ELB Unheathly"
+  insufficient_data_actions = []
+}
